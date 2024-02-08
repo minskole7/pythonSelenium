879c46f0244b1cb300ca0a24506e3f806bc235aa
minskole7 <minskole7@gmail.com>
2024-02-08 21:28:56 +0530

    added

---
+++ src2/test_six.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+import pytest
+driver = None
+@pytest.fixture(scope = 'module')
+def init_launch():
+    global  driver
+    driver = webdriver.Chrome()
+    driver.get("https://www.google.com")
+
+    yield
+    print("tear down")
+    driver.quit()
+
+
+@pytest.mark.usefixtures('init_launch')
+def test_verifyTitle():
+    assert driver.title == "Google"
+
+@pytest.mark.usefixtures('init_launch')
+def test_getCurrentUrl():
+    assert "google" in driver.current_url
+
+
+def test_getCurrentUrl2():
+    assert "https" in driver.current_url
+
+
+
